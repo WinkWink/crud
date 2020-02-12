9a18b1d9149dd5520ca7bd6b852c1913f8bdeff2
winkwink <amanda.winkler44@gmail.com>
2020-02-11 21:21:31 -0500

    add file structure

--- app.js
+++ app.js
@@ -3,3 +3,13 @@ const bodyParser = require('body-parser');
 
 // initialize our express app 
 const app = express();
+
+// tell express to listen to this port 
+
+let port = 1234;
+
+app.listen(port, () => {
+
+	console.log('server is up and running on port ' + port);
+
+});
